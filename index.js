8850f9540486e1625021166376a337b60aaaf9b9
Jamie Strausbaugh <jstrausb86@gmail.com>
2019-02-10 13:35:00 -0500

    Completed initial server request-response and tested by adding basic html content to the page in the response.

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-const Logger = require('./logger');
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
 
-const logger = new Logger();
+const server = http.createServer((req, res)  => {
+  if (req.url === '/') {
+    res.end('<h1>Home</h1>');
+  }
+});
 
-logger.on('message', data => console.log('Called Listener', data));
+const PORT = process.env.PORT || 5000;
 
-logger.log('Hello, World!');
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
