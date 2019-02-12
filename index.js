63bd81c988e77cb0c51d27700844c9e375c1da2f
Jamie Strausbaugh <jstrausb86@gmail.com>
2019-02-12 11:39:25 -0500

    Modified http server code to evaluate url request string and return html pages as well as api data in the form of a JSON object.

--- index.js
+++ index.js
@@ -4,7 +4,24 @@ const fs = require('fs');
 
 const server = http.createServer((req, res)  => {
   if (req.url === '/') {
-    res.end('<h1>Home</h1>');
+    fs.readFile(
+      path.join(__dirname, 'public', 'index.html'),
+      (err, content) => {
+        if (err) {
+          console.error(err);
+        }
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(content);
+      }
+    );
+  }
+  if (req.url === '/api/users') {
+    const users = [
+      {name: 'Jamie Strausbaugh', age: 32},
+      {name: 'Emily Devereaux', age: 24},
+    ];
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(users));
   }
 });
 
